60dea854f5f90fd81032665ce46da31eddfb520f
86135 <86135@DESKTOP-5L8OE6D>
2021-08-10 14:41:05 +0800

    111

--- src/com/leoch/sie/custom/utils/MyPerference.java
+++ src/com/leoch/sie/custom/utils/MyPerference.java
@@ -9,7 +9,6 @@ public class MyPerference {
 
 	private static final String OA_Address = "K8_OA_Address";// OA地址例：【http://192.168.1.145:88】
 	private static final String Smb_Address = "K8_Smb_Address";// 两个值【Send】【Get】用:隔开
-	private static final String PATH_Address = "K8_PATH_Address";// 获取tccs路径
 
 	private static TCPreferenceService service;
 
@@ -21,9 +20,6 @@ public class MyPerference {
 		return getStringValue(OA_Address);
 	}
 	
-	public static String getPATHAddress() throws TCException {
-		return getStringValue(PATH_Address);
-	}
 
 	public static TCPreferenceService getService() throws TCException {
 		if (service == null) {
